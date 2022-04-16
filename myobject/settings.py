5c21d13a4399672bb130434099000dd9d189e494
manojlamichhane <lammichhanemanoj1@gmail.com>
2022-04-15 22:52:00 -0400

    url_changes

--- myobject/settings.py
+++ myobject/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myadmin',
-    'web',
-
-   
+    'web',   
 ]
 
 MIDDLEWARE = [
@@ -60,7 +58,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates'),
-        os.path.join(BASE_DIR, 'web', 'templates', 'web'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -135,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
